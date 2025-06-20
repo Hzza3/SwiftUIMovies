36bb457e48ebc213d4b4b3c9a1aaf0607e57661a
A.Hazzaa <ahazzaa@arabdt.com>
2025-06-21 00:57:32 +0300

    Initial Commit

---
+++ SwiftUIMovies/SwiftUIMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoviesApp.swift
+//  SwiftUIMovies
+//
+//  Created by Ahmed Hazzaa on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
